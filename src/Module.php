4a7bc74add15b7920f70a19b1d37881aa1421573
Jenzri Nizar <jenzri.nizar.off@gmail.com>
2016-08-20 15:47:49 +0100

    Edit

---
+++ src/Module.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User: Jenzri Nizar
+ * Date: 19/08/2016
+ * Time: 14:21
+ */
+
+namespace Zf3\Minify;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\ModuleRouteListener;
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+
+    }
+
+}
